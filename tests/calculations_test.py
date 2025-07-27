12fab2371fe0f1e459fdbecffbb30efe7cb4d602
Wai Phyo Han <waiphyohn@gmail.com>
2025-07-27 19:57:49 +0000

    fix: test

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -70,21 +70,23 @@ def test_get_nth_fibonacci_ten():
    assert result == 55
 
 
-def test_area_of_circle_negative_radius(self):
+import pytest
+
+def test_area_of_circle_negative_radius():
    """Test with a negative radius to raise ValueError."""
    # Arrange
    radius = -1
 
    # Act & Assert
-   with self.assertRaises(ValueError):
+   with pytest.raises(ValueError):
       area_of_circle(radius)
 
 
-def test_get_nth_fibonacci_negative(self):
+def test_get_nth_fibonacci_negative():
    """Test with a negative number to raise ValueError."""
    # Arrange
    n = -1
 
    # Act & Assert
-   with self.assertRaises(ValueError):
+   with pytest.raises(ValueError):
       get_nth_fibonacci(n)
